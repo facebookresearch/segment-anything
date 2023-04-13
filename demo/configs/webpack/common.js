3518c86b78b3bc9cf4fbe3d18e682fad1c79dc51
SpencerWhitehead <srwhitehead31@gmail.com>
2023-04-13 08:51:39 -0700

    Update demo copyright headers.

--- demo/configs/webpack/common.js
+++ demo/configs/webpack/common.js
@@ -1,3 +1,9 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// All rights reserved.
+
+// This source code is licensed under the license found in the
+// LICENSE file in the root directory of this source tree.
+
 const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
--- demo/configs/webpack/dev.js
+++ demo/configs/webpack/dev.js
@@ -1,3 +1,9 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// All rights reserved.
+
+// This source code is licensed under the license found in the
+// LICENSE file in the root directory of this source tree.
+
 // development config
 const { merge } = require("webpack-merge");
 const commonConfig = require("./common");
