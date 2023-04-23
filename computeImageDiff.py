6a8870a17f45f4c91b3793197c7a089c553a08ad
james <jedarby@shaw.ca>
2023-04-23 14:07:40 -0400

    image comparison script

--- computeImageDiff.py
+++ computeImageDiff.py
@@ -2,11 +2,11 @@ import cv2
 import os
 
 fileName = "06052.tif"
-file_path = "../fullScrollDataTest/" + fileName
+file_path = "../../fullScrollDataTest/" + fileName
 
 # Load the images
 image1 = cv2.imread(file_path)
-image2 = cv2.imread("../losslesslyCompressedScrollData/c" + fileName)
+image2 = cv2.imread("../../losslesslyCompressedScrollData/c" + fileName)
 # Ensure the images have the same size and number of channels
 if image1.shape != image2.shape:
     raise ValueError("The two images must have the same size and number of channels.")
@@ -15,7 +15,7 @@ if image1.shape != image2.shape:
 diff_image = cv2.absdiff(image1, image2)
 
 # Save the resulting image
-outputFilePath = "../comparisonScrollData/comp" + fileName
+outputFilePath = "../../comparisonScrollData/comp" + fileName
 cv2.imwrite(
     outputFilePath,
     cv2.cvtColor(diff_image, cv2.COLOR_RGB2BGR),
