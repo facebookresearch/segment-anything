bd5c3f557b70ee0c33eea66c168f29ecf0a69d6a
Sagi Levi <sagi.l@cipia.com>
2023-05-28 13:47:55 +0300

    version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="segment_anything",
-    version="1.0",
+    version="1.1-cip",
     install_requires=[],
     packages=find_packages(exclude="notebooks"),
     extras_require={
