2e98fdf525aea5df761d9aa18031cae806eb365a
Rafael Silva <rafael@rafael.ai>
2024-04-08 00:14:27 -0400

    remove unneeded files for TorchScript port
    Removing unneeded files, so users don't get confused by
    what is necessary for the TorchScript version

--- segment_anything/__init__.py
+++ segment_anything/__init__.py
@@ -11,5 +11,3 @@ from .build_sam import (
     build_sam_vit_b,
     sam_model_registry,
 )
-from .predictor import SamPredictor
-from .automatic_mask_generator import SamAutomaticMaskGenerator
--- segment_anything/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
